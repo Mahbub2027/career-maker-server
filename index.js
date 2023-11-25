45d3f490960eadc92e519609bc8bf57e0eb7f82f
Mahbub2027 <alam201127@gmail.com>
2023-11-25 13:10:03 +0600

    services data insert

--- index.js
+++ index.js
@@ -26,6 +26,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const servicesCollection = client.db("careerMaker").collection("services");
+
+    // inset services
+    app.post('/services', async(req, res)=>{
+      const newServices = req.body;
+      console.log(newServices);
+      const result = await servicesCollection.insertOne(newServices);
+      res.send(result);
+    })
+
+    
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
